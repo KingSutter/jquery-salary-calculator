f87751bc5d58f541980493c0e563014050bca3ab
Will Sutter <kingsutter12@gmail.com>
2019-10-04 16:53:24 -0500

    create listers for all inputs

--- client.js
+++ client.js
@@ -1,14 +1,26 @@
 // init global variables
-let salary = []; // may not need. Try using .attr
+let first = '';
+let last = '';
+let id = '';
+let title = '';
+let salary = '';
+let salaries = []; // may not need. Try using .attr
 let total = 0;
 
 // on document ready...
 $(document).ready(main)
 // set up setters/getters
 function main(){
+  // submit button event
   $('#submitButton').on('click',submit)
 }
 
 function submit(){
   console.log('submit clicked!');
+  // getter for all inputs
+  first = $('#firstName').val();
+  last = $('#lastName').val();
+  id = $('#ID').val();
+  title = $('#title').val();
+  salary = $('#salary').val();
 }
